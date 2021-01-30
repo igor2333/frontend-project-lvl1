24656356fbed2ef0cead5714426ab71a3932f978
Igor <victororlov174@gmail.com>
2021-01-30 12:34:35 +0300

    add user greeting func

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync'
+
+const getUserName = () => {
+  const getUserNameQuestion = readlineSync.question('May I have your name? ')
+  return getUserNameQuestion
+}
+
+export default getUserName
