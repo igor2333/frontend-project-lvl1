59f101a6c8905f996959444ef2fb4fe723380487
Igor <victororlov174@gmail.com>
2021-02-11 14:56:25 +0300

    fix: brain-prime

--- games/prime.js
+++ games/prime.js
@@ -2,10 +2,11 @@ import gameProcess from '../src/index.js';
 import random from '../src/libs/random.js';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i + 1) {
-    if (num % i === 0) return 'no';
+  if (num < 2) return 'no';
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) return true;
   }
-  return 'yes';
+  return false;
 };
 
 const prime = () => {
@@ -14,7 +15,7 @@ const prime = () => {
   const questionAndCorrectAnswer = () => {
     const randomNumber = random(1, 100);
     const question = `Question: ${randomNumber}`;
-    const correctAnswer = isPrime(randomNumber);
+    const correctAnswer = isPrime(randomNumber) ? 'no' : 'yes';
     return [question, correctAnswer];
   };
 
